10b3b7a08c9cf849d0c1667cedce52877ff7784e
Alex Zhang <95546311+alexmingzhang@users.noreply.github.com>
2023-10-05 11:38:04 -0400

    Add test programs for ChunkyInt

---
+++ tests/chunkyint_miller_rabin_test.cpp
@@ -0,0 +1,54 @@
+#include <openssl/bn.h>
+
+#include <chrono>
+#include <iostream>
+
+#include "chunkyint.hpp"
+
+using namespace amzcrypto;
+
+int main() {
+    constexpr std::size_t num_iterations = 1000;
+    constexpr int num_checks = 40;
+    std::size_t num_successful = 0;
+
+    std::chrono::nanoseconds openssl_elapsed{};
+    std::chrono::nanoseconds chunkyint_elapsed{};
+
+    for (std::size_t i = 0; i < num_iterations; ++i) {
+        if (i % 100 == 0) {
+            std::cout << num_successful << "/" << i
+                      << " successful ChunkyInt modular exponentiation "
+                         "operations.\n";
+        }
+
+        ChunkyInts::ChunkyInt p;
+        p.randomize_bits(2048);
+
+        auto start = std::chrono::high_resolution_clock::now();
+        bool is_prime_1 = p.is_prime();  // OpenSSL primality check
+        auto stop = std::chrono::high_resolution_clock::now();
+        openssl_elapsed += stop - start;
+
+        start = std::chrono::high_resolution_clock::now();
+        bool is_prime_2 =
+            p.miller_rabin(num_checks);  // ChunkyInt primality check
+        stop = std::chrono::high_resolution_clock::now();
+        chunkyint_elapsed += stop - start;
+
+        if (is_prime_1 == is_prime_2) {
+            ++num_successful;
+        } else {
+            std::cerr << p << " is " << (is_prime_1 ? "prime" : "composite")
+                      << " but miller rabin-failed!\n\n";
+        }
+    }
+
+    std::cout << num_successful << "/" << num_iterations
+              << " successful miller-rabin checks.\n";
+
+    std::cout << "OpenSSL miller-rabin:   " << openssl_elapsed << '\n';
+    std::cout << "ChunkyInt miller-rabin: " << chunkyint_elapsed << '\n';
+
+    return 0;
+}
---
+++ tests/chunkyint_mod_exp_test.cpp
@@ -0,0 +1,61 @@
+#include <openssl/bn.h>
+
+#include <chrono>
+#include <iostream>
+
+#include "chunkyint.hpp"
+
+using namespace amzcrypto;
+
+int main() {
+    constexpr std::size_t num_iterations = 1000;
+    std::size_t num_successful = 0;
+
+    std::chrono::nanoseconds mod_exp_elapsed{};
+    std::chrono::nanoseconds mod_exp_cool_elapsed{};
+
+    ChunkyInts::ChunkyInt b;
+    ChunkyInts::ChunkyInt e;
+    ChunkyInts::ChunkyInt m;
+
+    for (std::size_t i = 0; i < num_iterations; ++i) {
+        if (i % 100 == 0) {
+            std::cout << num_successful << "/" << i
+                      << " successful ChunkyInt modular exponentiation "
+                         "operations.\n";
+        }
+
+        b.randomize_bits(2048);
+        e.randomize_bits(2048);
+        m.randomize_bits(1024);
+
+        auto start = std::chrono::high_resolution_clock::now();
+        ChunkyInts::ChunkyInt res1 = ChunkyInts::mod_exp(b, e, m);
+        auto stop = std::chrono::high_resolution_clock::now();
+        mod_exp_elapsed += stop - start;
+
+        start = std::chrono::high_resolution_clock::now();
+        ChunkyInts::ChunkyInt res2 = ChunkyInts::mod_exp_cool(b, e, m);
+        stop = std::chrono::high_resolution_clock::now();
+        mod_exp_cool_elapsed += stop - start;
+
+        bool equals = res1 == res2;
+
+        if (equals) {
+            ++num_successful;
+        } else {
+            std::cerr << res1 << " != " << res2 << "\n\n";
+        }
+    }
+
+    std::cout << num_successful << "/" << num_iterations
+              << " successful ChunkyInt modular exponentiation "
+                 "operations.\n";
+
+    std::cout << "OpenSSL modular exponentiation: " << mod_exp_elapsed << '\n';
+
+    std::cout << "ChunkyInt modular exponentiation: " << mod_exp_cool_elapsed
+              << '\n';
+
+    return 0;
+}
